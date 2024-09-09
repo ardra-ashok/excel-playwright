fc5f29223cf9dfe0036fd6b74aa7ec5b83ec6efd
Ardra Ashok <ardraasok@yahoo.in>
2024-09-09 23:24:11 +1000

    excel read and write file

--- ExcelUtils/excelDemo.js
+++ ExcelUtils/excelDemo.js
@@ -1,16 +1,27 @@
 const ExcelJs = require('exceljs')
 
-async function excelTest() {
- const workbook = new ExcelJs.Workbook()
+async function writeExcelTest(searchText, replaceText, change, filePath) {
+  const workbook = new ExcelJs.Workbook()
 
- await workbook.xlsx.readFile('./testData.xlsx')
+  await workbook.xlsx.readFile('./testData.xlsx')
  const worksheet = workbook.getWorksheet('Sheet1')
- worksheet.eachRow((row, rowNumber) => {
-  row.eachCell((cell, colNumber) => {
-   console.log(cell.value); 
-  })
- })
-
+   output = await readExcel(worksheet,searchText)
+  const cell = worksheet.getCell(output.row, output.column+change.colChange)
+  cell.value = replaceText
+  await workbook.xlsx.writeFile(filePath)
 }
 
-excelTest()
+async function readExcel(worksheet, searchText) {
+ let output = { row: -1, column: -1 }
+ worksheet.eachRow((row, rowNumber) => {
+  row.eachCell((cell, colNumber) => {
+   if (cell.value === searchText) {
+    output.row = rowNumber
+    output.column = colNumber
+   }
+  })
+ })
+ return output
+}
+
+writeExcelTest('Mango', '350',{rowChange:0,colChange:2},'./testData.xlsx')
